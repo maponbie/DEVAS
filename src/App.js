b4fa0e94c5379755111063ff57d39c68e61998d3
Martha Aponbiede <105121021+maponbie@users.noreply.github.com>
2024-02-03 12:57:49 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Companies from "./components/Companies/Companies";
-import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Companies from "./components/Companies/Companies";
+import Contact from "./components/Contact/Contact";
 import Donate from "./components/Donate/Donate";
 import Register from "./components/Register/Register";
 
