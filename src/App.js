bf5ea2c4d9c92fb606a8f6bb4c712203dcb5b3f3
Martha Aponbiede <105121021+maponbie@users.noreply.github.com>
2024-02-03 12:47:55 -0600

    Update App.js
    Added router info

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Companies from "./components/Companies/Companies";
 import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-import Donate from "./components//Donate/Donate";
+import Donate from "./components/Donate/Donate";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <Header />
-        <Hero />
+    <Router>
+      <div className="App">
+        <div>
+          <Header />
+          <Hero />
+        </div>
+        <Switch>
+          <Route path="/register" component={Register} />
+          <Route path="/companies" component={Companies} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/donate" component={Donate} />
+        </Switch>
       </div>
-      <Companies />
-      <Contact />
-      <Donate />
-    </div>
+    </Router>
   );
 }
 
 export default App;
+
