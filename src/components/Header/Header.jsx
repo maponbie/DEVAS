bcf0674207abb851dab9bcc464d54a4a84d77163
Martha Aponbiede <105121021+maponbie@users.noreply.github.com>
2024-02-03 12:56:59 -0600

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
-    <section class="h-wrapper">
+    <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
         <img src="./nourishnet.png" alt="logo" width={100} />
 
         <div className="flexCenter h-menu">
-          <a href="">Register Now</a>
-          <a href="">Residencies</a>
-          <a href="">Our Value</a>
+          <Link to="/register">Register Now</Link>
+          <Link to="/companies">Residencies</Link>
+          <Link to="/donate">Our Value</Link>
           <button className="button">
-            <a href="">Contact Us</a>
+            <Link to="/contact">Contact Us</Link>
           </button>
         </div>
       </div>
@@ -21,3 +22,4 @@ const Header = () => {
 };
 
 export default Header;
+
